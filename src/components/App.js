366cc4d3ee42681a26f836f1c47d46a83b313bee
Sungwoo97 <gwaja97@gmail.com>
2024-11-08 15:09:55 +0900

    회원가입 로그인 연동 완료

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { authService } from '../firebase';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import AppRouter from './Router';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+console.log(authService);
+
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [ init, setInit ] = useState(false);    //로그인 확인 시작 여부
+
+  useEffect(()=>{
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        //유저 정보가 있다면
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  },[])
+
   return (
-    <AppRouter/>
+    <>
+      {/* 로그인 확인 전에 메세지를 띄워주고 확인 이후 페이지를 보여준다 */}
+      {init ? <AppRouter isLoggedIn={isLoggedIn}/> : "Initializing..."}
+      
+    </>
   );
 }
 
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword  } from "firebase/auth";
 
 const Auth = ()=>{
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ newAccount, setNewAccount ] = useState(true);
+  const [ error, setError ] = useState('');
+
+  const auth = getAuth();
+
+
+
+ 
+  const onChange = (e)=>{
+    // const email = e.target.name;
+    // const password = e.target.name;
+    // const emailValue = e.target.email;
+    // const passwordValue = e.target.password;
+
+    const {target: {name, value}} = e;      // e.target 안의 name, value를 값을 변수로 지정
+    if(name === 'email'){
+      setEmail(value);
+    }else if ( name === 'password'){
+      setPassword(value);
+    }
+
+ 
+  }
+  const onSubmit = (e)=>{ 
+    e.preventDefault();
+    if(newAccount){
+      //회원가입
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // 계정 생성 완료 후 , 로그인 완료
+          const user = userCredential.user;   // 생성된 계정의 유저 확인
+          console.log(user);
+        })
+        .catch((error) => {
+          // const errorCode = error.code;
+          const errorMessage = error.message;
+          setError(errorMessage);
+        });
+    }else{
+      // 로그인
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        // const errorCode = error.code;
+        const errorMessage = error.message;
+        setError(errorMessage);
+      });
+    }
+  }
+  const toggleAccount = ()=>{
+    setNewAccount(prev=>!prev);
+  }
   return(
-    <div>Auth</div>
+    <div className="container">
+      <h1>{newAccount ? 'Account' : 'Login' } Form</h1>
+      <Form onSubmit={onSubmit}>
+        <Form.Group className="mb-3" controlId="loginEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" name="email" onChange={onChange} placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="loginPW">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" name="password" onChange={onChange} />
+        </Form.Group>
+        <Button type="submit" variant="primary">{newAccount ? 'Account' : 'Login' }</Button>
+        <div className="mt-3">{error}</div>
+      </Form>
+      <hr/>
+      <Button  variant="secondary" onClick={toggleAccount}>{newAccount ? '로그인으로 전환' : '회원가입으로 전환' }</Button>
+    </div>
   )
 }
 export default Auth;
