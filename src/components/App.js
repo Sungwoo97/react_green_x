ec08340dc4bf420b14aee5cdec2c385cfdb37324
Sungwoo97 <gwaja97@gmail.com>
2024-11-08 11:18:39 +0900

    AppRouter Login frame

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import './App.css';
+import AppRouter from './Router';
 
 function App() {
   return (
-    <div>
-    </div>
+    <AppRouter/>
   );
 }
 
---
+++ src/components/Router.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Auth from '../routes/Auth';
+import Home from '../routes/Home';
+
+const AppRouter = ()=>{
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return(
+    <Routes>
+      { isLoggedIn ? 
+      <Route path='/' element={<Home/>} /> 
+      : 
+      <Route path='/' element={<Auth/>} /> 
+      }         
+    </Routes>
+  )
+}
+export default AppRouter;
